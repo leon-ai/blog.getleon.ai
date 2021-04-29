e96bf932748ac583de37e5f8b635b3f3b0793ef0
louistiti <louis.grenard@gmail.com>
2021-04-29 11:06:36 +0800

    style: use SVGs instead of emojies for theme toggler

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,7 +41,26 @@ const Layout = ({ location, title, children }) => {
               className="theme-toggler"
               onClick={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')}
             >
-              {theme === 'dark' ? '☀' : '🌙'}
+              {theme === 'dark' ? (
+                <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor"
+                     stroke-width="2" fill="none"
+                     stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+                  <circle cx="12" cy="12" r="5" />
+                  <line x1="12" y1="1" x2="12" y2="3" />
+                  <line x1="12" y1="21" x2="12" y2="23" />
+                  <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                  <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                  <line x1="1" y1="12" x2="3" y2="12" />
+                  <line x1="21" y1="12" x2="23" y2="12" />
+                  <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                  <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+                </svg>
+              ) :
+                <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
+                     stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+                  <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+                </svg>
+              }
             </button>
           )}
         </ThemeToggler>
