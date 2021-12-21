1fd1c973b531da47334a2efe3cd73307420643b5
louistiti <louis.grenard@gmail.com>
2021-12-21 21:18:10 +0800

    chore: add latest post social images

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
         short_name: 'LeonBlog',
         start_url: '/',
         background_color: '#000000',
-        theme_color: '#1C75DB',
+        theme_color: '#000000',
         display: 'minimal-ui',
         icon: 'src/images/logo.svg' // This path is relative to the root of the site.
       }
