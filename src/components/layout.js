7be4783080b78258e3286377fa32f221594c4a29
louistiti <louis.grenard@gmail.com>
2022-07-16 23:27:21 +0800

    fix: comply to JSX SVG attributes

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,8 +43,8 @@ const Layout = ({ location, title, children }) => {
             >
               {theme === 'dark' ? (
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor"
-                     stroke-width="2" fill="none"
-                     stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+                     strokeWidth="2" fill="none"
+                     strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1">
                   <circle cx="12" cy="12" r="5" />
                   <line x1="12" y1="1" x2="12" y2="3" />
                   <line x1="12" y1="21" x2="12" y2="23" />
@@ -57,7 +57,7 @@ const Layout = ({ location, title, children }) => {
                 </svg>
               ) :
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
-                     stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+                     strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1">
                   <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
                 </svg>
               }
